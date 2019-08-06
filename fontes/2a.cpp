5d07f96c8fc72e3703239fa38f6448217842cb6a
Lucas <lucas-m.p@hotmail.com>
2019-08-06 14:26:41 -0300

    Atualização do Lukita

---
+++ fontes/2a.cpp
@@ -0,0 +1,32 @@
+/* Alunos: Lucas Meneghelli e Vinicius Gasparini | Tarefa 2.a */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long exp(long a, long b)
+{
+    long long ans = 1;
+    int count = 0;
+
+    for(; count < b; ans*=a, count++);
+
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+    long a, b;
+
+    cout << "Algoritmo a:" << endl;
+    
+    cout << "Informe a:";
+    cin >> a;
+    
+    cout << "Informe b:";
+    cin >> b;
+
+    cout << exp(a,b) << endl;
+
+    return 0;
+}
